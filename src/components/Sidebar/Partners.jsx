d18a7599e7350527ed201780a3e071baa20d394a
ana <105382108+anavoli@users.noreply.github.com>
2024-09-27 16:49:43 +0200

    Update Partners.jsx

--- src/components/Sidebar/Partners.jsx
+++ src/components/Sidebar/Partners.jsx
@@ -27,7 +27,7 @@ const Recent = () => {
     <SidebarCard>
       <Wrapper>
         <Title title="Our Partners" />
-        <p>Building an open internet together.</p>
+        <p>Building an open+++++++++ internet together.</p>
         {partners.map(partner => {
           return (
             <div key={partner.name} className="partner">
