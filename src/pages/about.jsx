75d8097146f88ffc050bbff43771ede077d75402
ana <105382108+anavoli@users.noreply.github.com>
2024-09-27 16:03:24 +0200

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,8 +9,8 @@ const about = () => {
   return (
     <Layout>
       <Seo
-        title="About Us | Revista"
-        description="Revista is a platform for passionate people to share knowledge. We are open source and believe in free learning."
+        title=" O nama | Neško "
+        description="Moja viđenja o nekim stvarima."
       />
 
       <AboutUs />
